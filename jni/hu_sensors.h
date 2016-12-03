af618ec750bdfd891dc253fd74d0b78e71f40446
Jernej Virag <jernej@virag.si>
2016-12-03 23:03:25 +0100

    Switch to protobufs for sensor event generation

--- jni/hu_sensors.h
+++ jni/hu_sensors.h
@@ -3,18 +3,35 @@
 #ifndef HU_SENSORS_H
 #define HU_SENSORS_H
 
+#include "pb_encode.h"
+#include "gen/sensors.pb.h"
+
+static bool pb_encode_nightmode(pb_ostream_t *stream, const pb_field_t* field, void* const *arg) {
+    if (!pb_encode_tag_for_field(stream, field)) {
+        return false;
+    }
+
+    int nightmode = *((int*)*arg);
+    HU_SensorEvent_DayNightMode dnm = HU_SensorEvent_DayNightMode_init_zero;
+    dnm.isNight = nightmode;
+    return pb_encode_submessage(stream, HU_SensorEvent_DayNightMode_fields, &dnm);
+}
+
 static int hu_fill_night_mode_message(uint8_t* buffer, int buffer_len, int nightmode) {
-    // Size sanity check
     if (buffer_len < 6) return 0;
 
-    int buffCount = 0;
-    buffer[buffCount++] = 0x80;
-    buffer[buffCount++] = 0x03;
-    buffer[buffCount++] = 0x52;
-    buffer[buffCount++] = 0x02;
-    buffer[buffCount++] = 0x08;
-    buffer[buffCount++] = (nightmode ? 0x01 : 0x00);
-    return buffCount;
+    // Write header first
+    buffer[0] = 0x80;
+    buffer[1] = 0x03;   // Touch event
+
+    pb_ostream_t ostream = pb_ostream_from_buffer(buffer + 2, buffer_len - 2);
+    HU_SensorEvent event = HU_SensorEvent_init_zero;
+    event.nightMode.funcs.encode = &pb_encode_nightmode;
+    event.nightMode.arg = &nightmode;
+    bool status = pb_encode(&ostream, HU_SensorEvent_fields, &event);
+
+    if (!status) return 0;
+    return 2 + ostream.bytes_written;
 }
 
 #endif
